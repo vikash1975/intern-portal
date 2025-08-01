49e532c0a45ca1111d99b3c8999494275c6d1b4d
vikash1975 <vikashpal2806@gmail.com>
2025-08-01 10:59:41 +0530

    Initial commit

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA-WqUzjc0xpkjfpzZNBfIJ40sXBD9zp3M",
+  authDomain: "intern-portal-6166d.firebaseapp.com",
+  projectId: "intern-portal-6166d",
+  storageBucket: "intern-portal-6166d.firebasestorage.app",
+  messagingSenderId: "661729688799",
+  appId: "1:661729688799:web:6e325e2e33c3064c63bd6a"
+};
+ firebase.initializeApp(firebaseConfig);
